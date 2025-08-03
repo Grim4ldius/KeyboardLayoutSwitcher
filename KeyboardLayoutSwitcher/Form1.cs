73f5145a394113a431b8655e4fedeabb25802d35
Grim4ldius <xavierminotte@gmail.com>
2025-08-03 13:22:28 +0200

    Change winform to wpf

--- KeyboardLayoutSwitcher/Form1.cs
+++
@@ -1,9 +0,0 @@
-namespace KeyboardLayoutSwitcher;
-
-public partial class Form1 : Form
-{
-    public Form1()
-    {
-        InitializeComponent();
-    }
-}
---
+++ KeyboardLayoutSwitcher/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace KeyboardLayoutSwitcher
+{
+    public partial class MainWindow : Window
+    {
+        private readonly LayoutSwitcher layoutSwitcher;
+        public ObservableCollection<AppLayoutItem> AppLayouts { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            layoutSwitcher = new LayoutSwitcher();
+            AppLayouts = new ObservableCollection<AppLayoutItem>(
+                layoutSwitcher.AppLayouts.Select(kvp => new AppLayoutItem { Application = kvp.Key, Layout = kvp.Value })
+            );
+            AppsGrid.ItemsSource = AppLayouts;
+
+            RefreshRunningApps();
+        }
+
+        private void RefreshRunningApps()
+        {
+            RunningAppsCombo.Items.Clear();
+            var processes = Process.GetProcesses()
+                .Select(p => p.ProcessName.ToLower() + ".exe")
+                .Distinct()
+                .OrderBy(n => n)
+                .ToList();
+            foreach (var proc in processes)
+                RunningAppsCombo.Items.Add(proc);
+        }
+
+        private void AddAppButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (RunningAppsCombo.SelectedItem is string appName && !string.IsNullOrWhiteSpace(appName))
+            {
+                AppLayouts.Add(new AppLayoutItem { Application = appName, Layout = "00000409 (QWERTY US)" });
+            }
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            layoutSwitcher.AppLayouts.Clear();
+            foreach (var item in AppLayouts)
+            {
+                var layoutHex = item.Layout.Split(' ')[0];
+                layoutSwitcher.AppLayouts[item.Application] = layoutHex;
+            }
+            layoutSwitcher.SaveLayouts();
+            MessageBox.Show("Associations sauvegardées !");
+        }
+
+        private void ReloadButton_Click(object sender, RoutedEventArgs e)
+        {
+            layoutSwitcher.LoadLayouts();
+            AppLayouts.Clear();
+            foreach (var kvp in layoutSwitcher.AppLayouts)
+            {
+                AppLayouts.Add(new AppLayoutItem { Application = kvp.Key, Layout = kvp.Value });
+            }
+            RefreshRunningApps();
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (AppsGrid.SelectedItem is AppLayoutItem item)
+            {
+                AppLayouts.Remove(item);
+            }
+        }
+
+        private void TestSwitchButton_Click(object sender, RoutedEventArgs e)
+        {
+            KeyboardManager.SwitchLayout("00000409"); // QWERTY US
+        }
+
+        private void TestSwitchFrButton_Click(object sender, RoutedEventArgs e)
+        {
+            KeyboardManager.SwitchLayout("0000040C"); // AZERTY FR
+        }
+    }
+
+    public class AppLayoutItem
+    {
+        public string Application { get; set; }
+        public string Layout { get; set; }
+    }
+}
